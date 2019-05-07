4316d61b2c1abcbf088361051de17308b2904361
雪猫 <SnowCait@users.noreply.github.com>
2019-05-07 12:23:20 +0900

    Update friendships.sql
    パーティション

--- tables/friendships.sql
+++ tables/friendships.sql
@@ -11,12 +11,16 @@ CREATE TABLE `friendships_logs` (
 	`action` TINYINT UNSIGNED NOT NULL,  # フォロー、解除（申請、承認、解除）
 	`created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (`following`, `followed`, `action`, `created_at`),
-	KEY (`followed`, `following`, `action`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+	KEY (`followed`, `following`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+PARTITION BY RANGE COLUMNS (`created_at`) (
+	PARTITION p20190101 VALUES LESS THAN ('2019-01-02 00:00:00'),
+	PARTITION p20190102 VALUES LESS THAN ('2019-01-03 00:00:00')
+);
 
 # フォローする（申請制の場合はフォローを返すことで承認とみなす）
 INSERT INTO `friendships` (`following`, `followed`) VALUES (?, ?);
-INSERT INTO `friendships_logs` (`action`, `following`, `followed`) VALUES (?, ?, ?);
+INSERT INTO `friendships_logs` (`following`, `followed`, `action`) VALUES (?, ?, ?);
 
 # フォローを外す（申請制の場合は両方解除する）
 DELETE FROM `friendships` WHERE `following` = ? AND `followed` = ?;
@@ -43,5 +47,33 @@ SELECT `a`.`following` FROM `friendships` AS `a`
 LEFT JOIN `friendships` AS `b` ON `a`.`following` = `b`.`followed` AND `a`.`followed` = `b`.`following`
 WHERE `b`.`following` IS NULL AND `a`.`followed` = ?;
 
+# パーティションの追加
+ALTER TABLE `friendships_logs` ADD PARTITION (
+	PARTITION p20190103 VALUES LESS THAN ('2019-01-04 00:00:00')
+);
+
+# パーティションのバックアップ 3 パターン（ $user, $db は任意の値、ファイルを見失わないように絶対パスにしておく）
+SELECT * FROM `friendships_logs` INTO OUTFILE '/var/lib/mysql-files/friendships_logs.p20190101.tsv';
+# mysqldump -u$user -p -hlocalhost --default-character-set=utf8 --complete-insert --no-create-info --where="created_at < '2019-01-02 00:00:00'" $db friendships_logs > /var/lib/mysql-files/friendships_logs.p20190101.sql
+# mysqldump -u$user -p -hlocalhost --where="created_at < '2019-01-02 00:00:00'" --tab=/var/lib/mysql-files/ $db friendships_logs
+
+## 下記ディレクトリに出力推奨
+SELECT @@secure_file_priv;
+# +-----------------------+
+# | @@secure_file_priv    |
+# +-----------------------+
+# | /var/lib/mysql-files/ |
+# +-----------------------+
+
+# パーティションの削除
+ALTER TABLE `friendships_logs` DROP PARTITION p20190101;
+
+# リストア 3 パターン
+LOAD DATA INFILE '/var/lib/mysql-files/friendships_logs.p20190101.tsv' INTO TABLE `friendships_logs`;
+# mysql -u$user -p -hlocalhost $db < /var/lib/mysql-files/friendships_logs.p20190101.sql
+# mysqlimport -u$user -p -hlocalhost $db /var/lib/mysql-files/friendships_logs.p20190101.tsv
+
 # NOTE
+# RANGE COLUMNS が使えるのは MySQL 5.5 以降
+# パーティションのドロップはデータ量に関係なく一瞬のようなので日単位ではなく月単位くらいでも良さそう。
 # 水平分割する場合は JOIN をやめて following で分ける。 Redis のリストか何かでキャッシュする。
