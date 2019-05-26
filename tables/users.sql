1743585280a01cbc0f7b937122c80063d24a4707
雪猫 <SnowCait@users.noreply.github.com>
2019-05-26 14:45:58 +0900

    Update users.sql

--- tables/users.sql
+++ tables/users.sql
@@ -1,10 +1,22 @@
 CREATE TABLE `users` (  # or accounts
 	`id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-	`name` VARCHAR(50) NOT NULL,  # 50文字
 	`screen_name` VARCHAR(15) NOT NULL,  # 15文字
 	`password_hash` VARCHAR(255) NOT NULL,  # 255文字
 	`phone_number` BIGINT,
 	`email` VARCHAR(255),
+	`created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `profiles` (
+	`id` BIGINT UNSIGNED NOT NULL,
+	`name` VARCHAR(50) NOT NULL,
+	`introduction` VARCHAR(160),
+	`place` VARCHAR(30),
+	`website_url` VARCHAR(100),
+	`birthday` DATETIME,
+	`icon_url` VARCHAR(100),
+	`header_url` VARCHAR(100),
 	`created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
